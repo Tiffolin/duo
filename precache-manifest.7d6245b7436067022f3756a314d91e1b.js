8a1c4b79a919bc14b7eeda302648f97a9b3dc24f
Tiffo <szuchinlin3@gmail.com>
2020-05-30 14:57:35 -0400

    Updates

---
+++ precache-manifest.7d6245b7436067022f3756a314d91e1b.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6831ac82de0670e54d3d72915925f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851c7b9f115626e966f3",
+    "url": "/static/css/main.7a27ab87.chunk.css"
+  },
+  {
+    "revision": "bd2a729bcb235b816aa5",
+    "url": "/static/js/2.cc3840c3.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/static/js/2.cc3840c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851c7b9f115626e966f3",
+    "url": "/static/js/main.2d7827bb.chunk.js"
+  },
+  {
+    "revision": "5e1a8d701390fc90315f",
+    "url": "/static/js/runtime-main.39dd0c14.js"
+  },
+  {
+    "revision": "b66ba13eb10c1fe3fef8c7cd97354e49",
+    "url": "/static/media/1.b66ba13e.png"
+  },
+  {
+    "revision": "a2c070cc683d172e7b1dc6f84f8fc04a",
+    "url": "/static/media/2.a2c070cc.png"
+  },
+  {
+    "revision": "cbb2f738c8b3134b3992093bc5761c20",
+    "url": "/static/media/3.cbb2f738.png"
+  },
+  {
+    "revision": "09c330b6e393ff6f4a46dcb0dba85fe4",
+    "url": "/static/media/4.09c330b6.png"
+  },
+  {
+    "revision": "2ff92d9dce4c2e2a6e6798889f6872b6",
+    "url": "/static/media/NoahLight.2ff92d9d.ttf"
+  },
+  {
+    "revision": "4518e85d0694229694798a37015abba9",
+    "url": "/static/media/almondcroissant.4518e85d.png"
+  },
+  {
+    "revision": "d8fa0415e6db0b8868f79dea838d188c",
+    "url": "/static/media/almondcroissant.d8fa0415.png"
+  },
+  {
+    "revision": "ebd817858379ab6b46ea117e5989c697",
+    "url": "/static/media/apple.ebd81785.png"
+  },
+  {
+    "revision": "ae49a8d3dd1f5b26a9a35fc3b545f457",
+    "url": "/static/media/applecroissant.ae49a8d3.png"
+  },
+  {
+    "revision": "8417f49ff7a37d9311e97c3edad138cb",
+    "url": "/static/media/avery.8417f49f.png"
+  },
+  {
+    "revision": "8458ae5824cc383dc13ef5dbe0c2baa8",
+    "url": "/static/media/baba.8458ae58.png"
+  },
+  {
+    "revision": "dd81a91a37bf73c6425b3ab23f65f1ee",
+    "url": "/static/media/banane.dd81a91a.png"
+  },
+  {
+    "revision": "f070dc4e1446495013407922e241b91c",
+    "url": "/static/media/bichon.f070dc4e.png"
+  },
+  {
+    "revision": "31075439b9544ad0c12d4ea7806a6072",
+    "url": "/static/media/blackforest.31075439.png"
+  },
+  {
+    "revision": "6c3be57e17528673fb34fb99ba71ee2a",
+    "url": "/static/media/blackforest.6c3be57e.png"
+  },
+  {
+    "revision": "abd4c3abeef5c6ee05dab827c7cef153",
+    "url": "/static/media/blackforest.abd4c3ab.png"
+  },
+  {
+    "revision": "f3277a2a64431c67567b2df8ccc9a38a",
+    "url": "/static/media/blackforest.f3277a2a.png"
+  },
+  {
+    "revision": "81583f85f88750d844afdda341e040bd",
+    "url": "/static/media/blacksesamebrioche.81583f85.png"
+  },
+  {
+    "revision": "3b0f4f0f9ca30cc632ab8144c3233ac5",
+    "url": "/static/media/bonbon.3b0f4f0f.png"
+  },
+  {
+    "revision": "2406fac8be6f5c7cc3e133dfb6b61c7b",
+    "url": "/static/media/bonbons.2406fac8.png"
+  },
+  {
+    "revision": "a5be73316e4d790507735e19acdb459c",
+    "url": "/static/media/briocheloaf.a5be7331.png"
+  },
+  {
+    "revision": "f099e0a55fc128821a5b7e30a309e4a5",
+    "url": "/static/media/briochetete.f099e0a5.png"
+  },
+  {
+    "revision": "10b2b01e8b374c4759bd1ba2b31e406f",
+    "url": "/static/media/c4.10b2b01e.png"
+  },
+  {
+    "revision": "353992d4e34ece91d76ced70d9eda851",
+    "url": "/static/media/c4.353992d4.png"
+  },
+  {
+    "revision": "2f051a195e90093d3455df1fe0fdade0",
+    "url": "/static/media/cadaeu.2f051a19.png"
+  },
+  {
+    "revision": "c75f3537c044be58fab35dd04fa32379",
+    "url": "/static/media/caffeineFree.c75f3537.png"
+  },
+  {
+    "revision": "5287f93f0746dce79cdf560e27e43ce3",
+    "url": "/static/media/canele.5287f93f.png"
+  },
+  {
+    "revision": "11750a72a02d67c1bc961419223c293f",
+    "url": "/static/media/cheddargreyure.11750a72.png"
+  },
+  {
+    "revision": "12bfa8ff88bf1dea5b7c28ce7aa6e018",
+    "url": "/static/media/chestnut.12bfa8ff.png"
+  },
+  {
+    "revision": "baa36b3f51a0d7dd38036351b9aeeda6",
+    "url": "/static/media/chestnut.baa36b3f.png"
+  },
+  {
+    "revision": "b03236ef98e54552f88f8928aac0ac8b",
+    "url": "/static/media/choco.b03236ef.png"
+  },
+  {
+    "revision": "39c04da571d9d487f40504c1f8fcba48",
+    "url": "/static/media/chocoalmond.39c04da5.png"
+  },
+  {
+    "revision": "c9e994e2b275fc92428e4bb196738da6",
+    "url": "/static/media/chocobook.c9e994e2.png"
+  },
+  {
+    "revision": "21667f43a941da5c2d823f427c0bbade",
+    "url": "/static/media/cinnamonrasonbrioche.21667f43.png"
+  },
+  {
+    "revision": "46fe00375540ef8df58884b769242096",
+    "url": "/static/media/citrus.46fe0037.png"
+  },
+  {
+    "revision": "d2774d380cd82d4f8579c30bc24b9f65",
+    "url": "/static/media/citrusbrioche.d2774d38.png"
+  },
+  {
+    "revision": "5b913fc9d57f67c7e98e7145e71c24bb",
+    "url": "/static/media/cream.5b913fc9.png"
+  },
+  {
+    "revision": "c483ab6cc2c708d8765de80f066ded2f",
+    "url": "/static/media/drinks.c483ab6c.png"
+  },
+  {
+    "revision": "5b25060102570020761cb271320aa493",
+    "url": "/static/media/email.5b250601.png"
+  },
+  {
+    "revision": "30c44522399cad67d47fa2b94faf6231",
+    "url": "/static/media/fei.30c44522.png"
+  },
+  {
+    "revision": "931cf7377a38aa6b4f5beb5b3ca0f50e",
+    "url": "/static/media/fei.931cf737.png"
+  },
+  {
+    "revision": "9f9c61ccf3525ca011412ba6cc7948d9",
+    "url": "/static/media/frangipane.9f9c61cc.png"
+  },
+  {
+    "revision": "dd01b7675fc05db83e07a438272845b7",
+    "url": "/static/media/fruit.dd01b767.png"
+  },
+  {
+    "revision": "3c26b49fd2ec1b9ed3eb81852e61310a",
+    "url": "/static/media/gabrielastencil-light.3c26b49f.ttf"
+  },
+  {
+    "revision": "904e8ecfff89b3af0e20db269c5e7b39",
+    "url": "/static/media/garden.904e8ecf.png"
+  },
+  {
+    "revision": "8f40c91d690ef691feb4017207624067",
+    "url": "/static/media/gb.8f40c91d.png"
+  },
+  {
+    "revision": "0fb0b2ad98c8e12bdd04a9e589227b46",
+    "url": "/static/media/hamandcheese.0fb0b2ad.png"
+  },
+  {
+    "revision": "5f9faa91d266653c4d5c75d769860583",
+    "url": "/static/media/hazel.5f9faa91.png"
+  },
+  {
+    "revision": "6d4fd2dad2cbd6bfee9496aacec374d5",
+    "url": "/static/media/honeytwist.6d4fd2da.png"
+  },
+  {
+    "revision": "21c051b887a715ba845dd953b75f32be",
+    "url": "/static/media/latinotype-gabriela-stencil-bold.21c051b8.ttf"
+  },
+  {
+    "revision": "ae6f274ac49c7a08f448769b6ff8c1cf",
+    "url": "/static/media/lemontart.ae6f274a.png"
+  },
+  {
+    "revision": "1b9b55984f56d303eeb738cd62612a92",
+    "url": "/static/media/logo1.1b9b5598.png"
+  },
+  {
+    "revision": "88d52a8309dad449e5d7174c078ebacd",
+    "url": "/static/media/logo1.88d52a83.png"
+  },
+  {
+    "revision": "d8c8da8dca51a0e1f8e5776f0da5841f",
+    "url": "/static/media/matcha.d8c8da8d.png"
+  },
+  {
+    "revision": "b2ec3a9890515687ba0bdcc4ad1d14a9",
+    "url": "/static/media/matchabook.b2ec3a98.png"
+  },
+  {
+    "revision": "271d423297519ba677cfece4ec6e3b09",
+    "url": "/static/media/mb.271d4232.png"
+  },
+  {
+    "revision": "885e0a2f83f6bb802f05ac4db8540558",
+    "url": "/static/media/mixberries.885e0a2f.png"
+  },
+  {
+    "revision": "ac85a3b2d92dcc692f960e5156ffbf06",
+    "url": "/static/media/mixberries.ac85a3b2.png"
+  },
+  {
+    "revision": "a96b57cdf7bbdf22a8078c770e9999ea",
+    "url": "/static/media/mona.a96b57cd.png"
+  },
+  {
+    "revision": "56f1923d9d80896f682bc09ec249229c",
+    "url": "/static/media/nova.56f1923d.png"
+  },
+  {
+    "revision": "a8d31507780aa8bfee0c6fd3d359b5cb",
+    "url": "/static/media/panauchocolat.a8d31507.png"
+  },
+  {
+    "revision": "d86de28603b0e0967ccf22eb0611df2f",
+    "url": "/static/media/payment.d86de286.png"
+  },
+  {
+    "revision": "fc1826cc410720c5f1e17d2ff543b392",
+    "url": "/static/media/peacan.fc1826cc.png"
+  },
+  {
+    "revision": "aef89569f0a8be865fa46e408498eb1b",
+    "url": "/static/media/pearl.aef89569.png"
+  },
+  {
+    "revision": "29dcee644e0bed2664424bc10349c360",
+    "url": "/static/media/pistache.29dcee64.png"
+  },
+  {
+    "revision": "ffec781852c1030a49fbe48194e8f2e9",
+    "url": "/static/media/pistache.ffec7818.png"
+  },
+  {
+    "revision": "64504deb9c16ac145677b6753d21e3d5",
+    "url": "/static/media/pistachiobrioche.64504deb.png"
+  },
+  {
+    "revision": "a37b23efdd16827f84f405eae56bbe4a",
+    "url": "/static/media/plaincroissant.a37b23ef.png"
+  },
+  {
+    "revision": "58053d16e38d0a348e1a3bcfba8ba79e",
+    "url": "/static/media/polonaise.58053d16.png"
+  },
+  {
+    "revision": "c0496a494a2046d5b188dfb35c589c67",
+    "url": "/static/media/quiche.c0496a49.png"
+  },
+  {
+    "revision": "34d24558acc951fd4b334b660d7c3d4a",
+    "url": "/static/media/rasp.34d24558.png"
+  },
+  {
+    "revision": "5c2adb38b10d4bdd0def27624ed2a8eb",
+    "url": "/static/media/rasp.5c2adb38.png"
+  },
+  {
+    "revision": "7ff6d25bdffaf60840c2e2a49f72253d",
+    "url": "/static/media/raspberryrose.7ff6d25b.png"
+  },
+  {
+    "revision": "0ea7cc7554afd31a2881114e01c9df93",
+    "url": "/static/media/retail.0ea7cc75.png"
+  },
+  {
+    "revision": "3d8b2976dc87dd4a97f72daf42457e94",
+    "url": "/static/media/retail.3d8b2976.png"
+  },
+  {
+    "revision": "28bcfc0748956f186ee00d2ba4e831e1",
+    "url": "/static/media/roastbeef.28bcfc07.png"
+  },
+  {
+    "revision": "56fdba58a9d17b583456e9e9f26f9a78",
+    "url": "/static/media/salamiandcheese.56fdba58.png"
+  },
+  {
+    "revision": "ad782b1334ee4d78bed4eb755f94e19f",
+    "url": "/static/media/sicily.ad782b13.png"
+  },
+  {
+    "revision": "0e207dd85e592eb49e4dd91e066828f0",
+    "url": "/static/media/soleil.0e207dd8.png"
+  },
+  {
+    "revision": "4e88236003e2439ba57a33d2d78c742d",
+    "url": "/static/media/sthonore.4e882360.png"
+  },
+  {
+    "revision": "036efc5aa03bd52e2568271214b2d1d8",
+    "url": "/static/media/storeFront.036efc5a.png"
+  },
+  {
+    "revision": "92447a400161464f59c9403636b2ac5e",
+    "url": "/static/media/supernova.92447a40.png"
+  },
+  {
+    "revision": "972014838f2939297af14aafe4308891",
+    "url": "/static/media/tatin.97201483.png"
+  },
+  {
+    "revision": "95b97c4005d1f7046219f17f3b258130",
+    "url": "/static/media/tropezienne.95b97c40.png"
+  },
+  {
+    "revision": "a4908291149c4ab2177faccc4fb2a326",
+    "url": "/static/media/vanillacustard.a4908291.png"
+  },
+  {
+    "revision": "3927600481f007c80a3d594ce23e7064",
+    "url": "/static/media/virgo.39276004.png"
+  },
+  {
+    "revision": "b0571151c518ccf5dd617ed91d7f7ed0",
+    "url": "/static/media/wholeblackforest.b0571151.png"
+  },
+  {
+    "revision": "4748593327572ae9c7a645528506aca3",
+    "url": "/static/media/wholeblackforest1.47485933.png"
+  },
+  {
+    "revision": "9247a5247943c23f990c8c7dcb2c6d4a",
+    "url": "/static/media/yuco.9247a524.png"
+  },
+  {
+    "revision": "ce28dafbbe01b3bf2ca52d0d313b7c75",
+    "url": "/static/media/yuco.ce28dafb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53c71414cc2bc1d82ef917a8269fe5e0.js"
+  "/precache-manifest.7d6245b7436067022f3756a314d91e1b.js"
 );
 
 self.addEventListener('message', (event) => {
